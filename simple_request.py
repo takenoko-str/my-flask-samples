69b184a5b90066fd65158eb0cef8d5c9b8782ea9
takenoko-str <taku.hyogo.kei@gmail.com>
2018-08-25 10:14:01 +0900

    サンプル画像追加

--- simple_request.py
+++ simple_request.py
@@ -7,7 +7,7 @@ import requests
 # initialize the Keras REST API endpoint URL along with the input
 # image path
 KERAS_REST_API_URL = "http://localhost:5000/predict"
-IMAGE_PATH = "dog.jpg"
+IMAGE_PATH = "dog.png"
 
 # load the input image and construct the payload for the request
 image = open(IMAGE_PATH, "rb").read()
