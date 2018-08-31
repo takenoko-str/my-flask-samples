493e721491220733efa60ed46c7cc687f60519d1
takenoko-str <taku.hyogo.kei@gmail.com>
2018-08-31 14:48:53 +0900

    サンプルDBを参照するFLASK APIにした

--- flask_db_sample.py
+++ flask_db_sample.py
@@ -1,23 +1,25 @@
 import json
-from flask import Flask
+from flask import Flask, request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'mysql'
+app.config['MYSQL_DB'] = 'sakila'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
 
 
-@app.route('/')
-def users():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT user, host FROM user')
-    rv = cur.fetchall()
-    return json.dumps(rv, indent=4)
+@app.route('/id/<num>', methods=['GET'])
+def users(num):
+    if request.method == 'GET':
+        cur = mysql.connection.cursor()
+        cur.execute('select title, description from film where film_id = {}'.format(num))
+        rv = cur.fetchall()
+        return json.dumps(rv, indent=4)
+
 
 if __name__ == '__main__':
     app.run()
